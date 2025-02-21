0fcf75265f8625d9164df1816630578bde1e4101
Anton Fayfer <fayfer2004@mail.ru>
2025-02-21 17:14:17 +0600

    add db_cache values to settings

--- fastApi_Stepik/pomodoro-time/settings.py
+++ fastApi_Stepik/pomodoro-time/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DB_HOST: str = "0.0.0.0"
     DB_POST: int = 5432
     DB_USER: str = "postgres"
-    DB_PASS: str = "password"
+    DB_PASSWORD: str = "password"
     DB_NAME: str = "pomodoro"
     CACHE_HOST: str = "0.0.0.0"
     CACHE_PORT: int = 6379
