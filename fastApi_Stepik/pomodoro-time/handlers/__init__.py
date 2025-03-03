d7cd673fa5308279486e4416b7a96cbe4b3ad767
Anton Fayfer <fayfer2004@mail.ru>
2025-03-03 22:47:54 +0600

    fix github. Add auth google and yandex

--- fastApi_Stepik/pomodoro-time/handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from handlers.tasks import router as task_router
-from handlers.ping import router as ping_router
-from handlers.categories import router as category_router
-
-routers = [task_router, ping_router, category_router]
---
+++ repository/user.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from sqlalchemy import insert, select
+from sqlalchemy.orm import Session
+
+from dataclasses import dataclass
+from models import UserProfile
+from schema import UserCreateSchema
+
+
+@dataclass
+class UserRepository:
+    db_session: Session
+
+    def get_user_by_email(self, email: str) -> Optional[UserProfile]:
+        query = select(UserProfile).where(UserProfile.email == email)
+        with self.db_session() as session:
+            return session.execute(query).scalar_one_or_none()
+
+    def create_user(self, user: UserCreateSchema) -> UserProfile:
+        query = (
+            insert(UserProfile).values(**user.model_dump()).returning(UserProfile.id)
+        )
+        # model_dump превращает в словарь( с помощью ** распаковываем)
+        with self.db_session() as session:
+            user_id: int = session.execute(query).scalar()
+            session.commit()
+            session.flush()
+            return self.get_user(user_id)
+
+    def get_user(self, user_id) -> Optional[UserProfile]:
+        query = select(UserProfile).where(UserProfile.id == user_id)
+        with self.db_session() as session:
+            return session.execute(query).scalars().first()
+
+    def get_user_by_username(self, username: str) -> Optional[UserProfile]:
+        query = select(UserProfile).where(UserProfile.username == username)
+        with self.db_session() as session:
+            return session.execute(query).scalars().first()
